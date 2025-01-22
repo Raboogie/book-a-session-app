36775eae53fb9a4f8b3dad39fc925acf1e7b134d
Ray <Dynasty7@outlook.com>
2025-01-22 01:05:29 -0500

    Removed unused boolean from SessionContext.tsx

--- src/lib/SessionContext.tsx
+++ src/lib/SessionContext.tsx
@@ -1,71 +1,67 @@
-import React, {createContext} from "react";
+import React, { createContext } from 'react';
 
 export type SessionType = {
-    // // user inputs
-    // name: string;
-    // email: string;
+	// // user inputs
+	// name: string;
+	// email: string;
 
-    // session data
-    id: string;
-    title: string;
-    summary: string;
-    description: string;
-    duration: number;
-    date: string;
-    image: string;
-
+	// session data
+	id: string;
+	title: string;
+	summary: string;
+	description: string;
+	duration: number;
+	date: string;
+	image: string;
 };
 
 type SessionState = {
-    isBooked: boolean;
-    session: SessionType[];
+	session: SessionType[];
 };
 
 type SessionContextValue = SessionState & {
-    addSession: (session: SessionType) => void;
-    clearSession: () => void;
+	addSession: (session: SessionType) => void;
+	clearSession: (id: string) => void;
 };
 
 type sessionContextProviderProps = {
-    children: React.ReactNode;
+	children: React.ReactNode;
 };
 
 const BookedSessionContext = createContext<SessionContextValue | null>(null);
 
 export const useBookedSessionContext = () => {
-    const bookedSessionCtx = React.useContext(BookedSessionContext);
-    if (!bookedSessionCtx) {
-        throw new Error('useBookedSessionContext Should not be null!');
-    }
-    return bookedSessionCtx;
+	const bookedSessionCtx = React.useContext(BookedSessionContext);
+	if (!bookedSessionCtx) {
+		throw new Error('useBookedSessionContext Should not be null!');
+	}
+	return bookedSessionCtx;
 };
 
-const BookedSessionsContextProvider = ({children}: sessionContextProviderProps) => {
-    const [session, setSession] = React.useState<SessionType[]>([]);
-    const [isBooked, setIsBooked] = React.useState<boolean>(false);
+const BookedSessionsContextProvider = ({
+	children,
+}: sessionContextProviderProps) => {
+	const [session, setSession] = React.useState<SessionType[]>([]);
 
-    const addSession = (session: SessionType) => {
-        setSession((prevSessions) => [...prevSessions, session]);
-        setIsBooked(true);
-    };
+	const addSession = (session: SessionType) => {
+		setSession((prevSessions) => [...prevSessions, session]);
+	};
 
-    const clearSession = () => {
-        setSession([]);
-        setIsBooked(false);
-    };
+	const clearSession = (id: string) => {
+		setSession((prevSessions) => prevSessions.filter((s) => s.id !== id));
+	};
 
-    const contextValue: SessionContextValue = {
-        isBooked,
-        session,
-        addSession,
-        clearSession
-    };
+	const contextValue: SessionContextValue = {
+		session,
+		addSession,
+		clearSession
+	};
 
-    return (
-        <BookedSessionContext.Provider value={contextValue}>
-            {children}
-        </BookedSessionContext.Provider>
-    );
+	return (
+		<BookedSessionContext.Provider value={contextValue}>
+			{children}
+		</BookedSessionContext.Provider>
+	);
 };
 
 export default BookedSessionsContextProvider;
